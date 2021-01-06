dd412765bc0a01bddb8c48bf5d2b2ed0bc9d975f
fidel bello <59423@bears.smcc.edu>
2021-01-06 18:33:09 -0500

    made variables for the numbers, letters, and special char

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +13,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function writePassword(legnth, upper, lower, numeric, special){
 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
