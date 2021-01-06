fc3f1a057432fa9d3fa4c60c33b6c3cec2f63e3f
fidel bello <59423@bears.smcc.edu>
2021-01-06 18:39:41 -0500

    made declarations for the variables

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//arrays
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+//declarations
+var confirmLength = ""
+var confirmUpper;
+var confirmLower;
+var confirmSpecial;
+var confirmNumeric;
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
